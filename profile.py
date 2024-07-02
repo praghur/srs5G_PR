cac193a1819a27bcc1d17c6cd79bebf759d17e16
praghur <166060957+praghur@users.noreply.github.com>
2024-07-02 12:13:09 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -104,7 +104,7 @@ SRS_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-srs.sh")
 OPEN5GS_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-open5gs.sh")
 UBUNTU_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 DEFAULT_SRS_HASHES = {
-    "srsRAN_4G": "release_23_04_1",
+    "srsRAN_4G": "release_23_11",
     "srsRAN_Project": "release_24_04",
 }
 
