db5cb5d9602ac382779d8ce9829be9ee4ca42b86
praghur <166060957+praghur@users.noreply.github.com>
2024-07-03 10:40:35 -0400

    Added Node 1 IP address

--- profile.py
+++ profile.py
@@ -130,6 +130,8 @@ request = pc.makeRequestRSpec()
 node = request.RawPC("node")
 node.hardware_type = params.nodetype
 node.disk_image = UBUNTU_IMG
+iface1 = node1.addInterface("eth1")
+iface1.addAddress(rspec.IPv4Address("192.168.0.11", "255.255.255.0"))
 
 for srs_type, type_hash in DEFAULT_SRS_HASHES.items():
     cmd = "{} '{}' {}".format(SRS_DEPLOY_SCRIPT, type_hash, srs_type)
