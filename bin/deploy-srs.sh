a5bea2a57dfa72129dae7d272bf6ba8ee600ef93
praghur <166060957+praghur@users.noreply.github.com>
2025-01-25 13:33:32 -0500

    Update deploy-srs.sh

--- bin/deploy-srs.sh
+++ bin/deploy-srs.sh
@@ -11,6 +11,22 @@ if [ -f $SRCDIR/$SRS_TYPE-setup-complete ]; then
     exit 0
 fi
 
+# Get the emulab repo
+while ! wget -qO - http://repos.emulab.net/emulab.key | sudo apt-key add -
+do
+    echo Failed to get emulab key, retrying
+done
+
+while ! sudo add-apt-repository -y http://repos.emulab.net/powder/ubuntu/
+do
+    echo Failed to get johnsond ppa, retrying
+done
+
+while ! sudo apt-get update
+do
+    echo Failed to update, retrying
+done
+
 install_srsran_common () {
     sudo apt update
     sudo apt install -y \
@@ -85,5 +101,20 @@ else
     echo "unknown SRS_TYPE: $SRS_TYPE"
     exit 1
 fi
+
+echo configuring nodeb...
+mkdir -p $SRCDIR/etc/srsran
+cp -r $ETCDIR/srsran/* $SRCDIR/etc/srsran/
+LANIF=`ip r | awk '/192\.168\.1\.0/{print $3}'`
+if [ ! -z $LANIF ]; then
+  LANIP=`ip r | awk '/192\.168\.1\.0/{print $NF}'`
+  echo LAN IFACE is $LANIF IP is $LANIP.. updating nodeb config
+  find $SRCDIR/etc/srsran/ -type f -exec sed -i "s/LANIP/$LANIP/" {} \;
+  IPLAST=`echo $LANIP | awk -F. '{print $NF}'`
+  find $SRCDIR/etc/srsran/ -type f -exec sed -i "s/GNBID/$IPLAST/" {} \;
+else
+  echo No LAN IFACE.. not updating nodeb config
+fi
+echo configuring nodeb... done.
 
 touch $SRCDIR/$SRS_TYPE-setup-complete
