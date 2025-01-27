a09e79b49c673e9f0670c6b30faf38eedd5d6fed
praghur <166060957+praghur@users.noreply.github.com>
2025-01-27 14:59:14 -0500

    Update deploy-srs.sh

--- bin/deploy-srs.sh
+++ bin/deploy-srs.sh
@@ -32,7 +32,7 @@ cmake ../
 make -j `nproc`
 sudo make install
 sudo ldconfig
-touch $SRCDIR/$SRS_TYPE-installed_srsran_4G-complete
+touch $SRCDIR-installed_srsran_4G-complete
 
 #Install srsran_Project
 sudo apt update
@@ -61,65 +61,6 @@ cmake ../ -DENABLE_EXPORT=ON -DENABLE_ZEROMQ=ON
 make -j `nproc`
 sudo make install
 sudo ldconfig
-touch $SRCDIR/$SRS_TYPE-installed_srsran_project-complete
+touch $SRCDIR-installed_srsran_project-complete
 
-install_srsran_common () {
-    sudo apt update
-    sudo apt install -y \
-        cmake \
-        libfftw3-dev \
-        libmbedtls-dev \
-        libsctp-dev \
-        libzmq3-dev \
-        build-essential \
-        libboost-program-options-dev \
-        libconfig++-dev \
-    touch $SRCDIR/$SRS_TYPE-install_srsran_common-complete
-}
-
-clone_build_install () {
-    cd $SRCDIR
-    git clone $SRS_REPO #Choose https://github.com/srsran/srsRAN_4G OR https://github.com/srsran/srsRAN_Project
-    cd $SRS_TYPE
-    git checkout $COMMIT_HASH  #Choose "srsRAN_4G": "release_23_04_1" OR "srsRAN_Project": "4ac5300d4927b5199af69e6bc2e55d061fc33652"
-    mkdir build
-    cd build
-    if [ "$SRS_TYPE" = "srsRAN_Project" ]; then
-        cmake ../ -DENABLE_EXPORT=ON -DENABLE_ZEROMQ=ON
-    else
-        cmake ../
-    fi
-    make -j `nproc`
-    sudo make install
-    sudo ldconfig
-    touch $SRCDIR/$SRS_TYPE-clone_build_install-complete
-}
-
-install_srsran_4g () {
-    install_srsran_common
-    sudo apt install -y \
-        build-essential \
-        libboost-program-options-dev \
-        libconfig++-dev \
-
-    clone_build_install
-    sudo srsran_install_configs.sh service
-    sudo cp /local/repository/etc/srsran/* /etc/srsran/
-    touch $SRCDIR/$SRS_TYPE-install_srsran_4g-complete
-}
-
-install_srsran_project () {
-    install_srsran_common
-    sudo apt install -y \
-        make \
-        gcc \
-        g++ \
-        pkg-config \
-        libyaml-cpp-dev \
-        libgtest-dev
-
-    clone_build_install
-    touch $SRCDIR/$SRS_TYPE-install_srsran_project-complete
-}
-
-touch $SRCDIR/$SRS_TYPE-setup-completed-fully
+touch $SRCDIR-setup-completed-fully
