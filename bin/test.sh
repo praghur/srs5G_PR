bcc1144a8a74d5c9b1a74ca6d9b3607dfde41e17
praghur <166060957+praghur@users.noreply.github.com>
2025-01-28 11:29:27 -0500

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -12,3 +12,16 @@ sudo ip netns exec ue1 tcpdump -i tun_srsue -w ue1_capture.pcap
 
 sudo ip netns exec ue2 tshark -i tun_srsue -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue2_results.csv
 sudo ip netns exec ue2 tcpdump -i tun_srsue -w ue2_capture.pcap
+
+
+#Save results from CN
+scp praghur@pc816.emulab.net:cn_results.csv /home/ubuntu/
+scp praghur@pc816.emulab.net:ogstun_capture.pcap /home/ubuntu/
+
+#Save results from UE1
+scp praghur@pc06-fort.emulab.net:ue1_results.csv /home/ubuntu/
+scp praghur@pc06-fort.emulab.net:ue1_capture.pcap /home/ubuntu/
+
+#Save results from UE2
+scp praghur@pc07-fort.emulab.net:ue2_results.csv /home/ubuntu/
+scp praghur@pc07-fort.emulab.net:ue2_capture.pcap /home/ubuntu/
