9b5daaa7cf7d74aa9e9396bdb565ccbd7d1ed21f
praghur <166060957+praghur@users.noreply.github.com>
2025-01-28 11:15:43 -0500

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -7,8 +7,8 @@ sudo ip netns exec ue2 traceroute -U -f 2 -m 2 -p 33435 10.45.1.10
 sudo tshark -i ogstun -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > cn_results.csv
 sudo tcpdump -i ogstun -w ogstun_capture.pcap
 
-sudo tshark -i tun_srsue -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue1_results.csv
-sudo tcpdump -i tun_srsue -w ue1_capture.pcap
+sudo ip netns exec ue1 tshark -i tun_srsue -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue1_results.csv
+sudo ip netns exec ue1 tcpdump -i tun_srsue -w ue1_capture.pcap
 
-sudo tshark -i tun_srsue -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue2_results.csv
-sudo tcpdump -i tun_srsue -w ue2_capture.pcap
+sudo ip netns exec ue2 tshark -i tun_srsue -T fields -e frame.time_epoch -e ip.src -e ip.dst -e ip.id -e udp.srcport -e udp.dstport -E header=y -E separator=, -E quote=d > ue2_results.csv
+sudo ip netns exec ue2 tcpdump -i tun_srsue -w ue2_capture.pcap
