beba61dc923c99487aa863ff4d4582ee57216c8b
praghur <166060957+praghur@users.noreply.github.com>
2025-02-06 10:15:15 -0500

    Update loopscript.sh

--- etc/srsran/loopscript.sh
+++ etc/srsran/loopscript.sh
@@ -4,7 +4,7 @@
 run_traceroute() {
   for i in {1..50}
   do
-    result=$(traceroute -U -q 10 -f 2 -m 2 -p 33435 10.45.2.10)
+    result=$(traceroute -U -f 2 -m 2 -p 33435 10.45.2.10)
     echo "$(date), $result" >> traceroute_results.csv
     sleep $((RANDOM % 70 + 1))  # Sleep for a random time between 1 to 70 seconds
   done
